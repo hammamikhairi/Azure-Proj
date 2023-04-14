36b52d70f7b0178deacb61308fad73c6bbc95bee
HammamiKhairi <hammamikhairi@gmail.com>
2023-04-14 02:53:41 +0100

    typo

--- GetAll.py
+++ GetAll.py
@@ -2,7 +2,7 @@ from AzureClient import AzureClient
 from os          import environ
 
 if __name__ == "__main__" : 
-    client = AzureClient(environ.get("YOUR_SQL_SERVER_PASSWORD"))
+    client = AzureClient(environ.get("AZURE_DB_SECRET"))
 
     print("{:>30} {:30}".format("COUNTRY", "CAPITAL"))
     for row in client.exec_get("SELECT * FROM COUNTRIES_TABLE"):
